8bb14044c618009e939d55adfc407c77c33dd10b
Nguyen Duc Huy <nguyenduchuy1968@gmail.com>
2024-05-15 14:40:46 +0300

    main

--- configs/settings.py
+++ configs/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #my apps
-    'cars',
+
 ]
 
 MIDDLEWARE = [
--- configs/urls.py
+++ configs/urls.py
@@ -1,12 +1,5 @@
 from django.urls import path
-# from cars.views import CarTestView, CarDetailView
-from cars.views import CarListCreateView
 
 
 urlpatterns = [
-    # path('carsTest', CarTestView.as_view()),
-    # path('carDetail/<int:pk>', CarDetailView.as_view()),
-    # path('carDetail/<slug:pk>', CarDetailView.as_view()),
-    # path('admin/', admin.site.urls),
-    path('cars', CarListCreateView.as_view())
 ]
